238812d7e849e393fd57edc6dabcaabfd00333ab
griffith <xjulianbarragan@gmail.com>
2026-02-12 11:51:31 +0100

    initiated filtering i.e. part 3 of a1

---
+++ a1/filtering.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from skimage.utils import random_noise
+from skimage.filter.rank import median, mean
+
+from const import DATA_FOLDER
+from const import OUTPUT_FOLDER
+
+if __name__ == "__main__":
+    print("Hello from filtering.py")
