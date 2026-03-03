eebd8e7370bf3845ca21e4e8b52ee824620a171d
Oskar Linke <oskarlinke@Oskars-MacBook-Pro.local>
2026-03-03 09:47:13 +0100

    created const.py

---
+++ a5/const.py
@@ -0,0 +1,9 @@
+
+from pathlib import Path
+
+DATA_FOLDER = Path(__name__).parent / "data"
+OUTPUT_FOLDER = Path(__name__).parent / "a5" / "output"
+
+IMAGES_FOLDER = DATA_FOLDER / "images"
+SAMPLES_FOlDER = DATA_FOLDER / "sound_samples" 
+IMPULSES_FOLDER = DATA_FOLDER / "sound_impulses"
