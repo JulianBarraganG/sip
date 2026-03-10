5b2760f1187acb81681ba83b62c3a040fcce69bc
Oskar Linke <oskarlinke@Oskars-MacBook-Pro.local>
2026-03-10 08:43:17 +0100

    created a6 folder and const file

---
+++ a6/const.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+DATA_FOLDER = Path(__name__).parent / "data"
+OUTPUT_FOLDER = Path(__name__).parent / "a6" / "output"
+
+IMAGES_FOLDER = DATA_FOLDER / "images"
+SAMPLES_FOLDER = DATA_FOLDER / "sound-samples" 
+IMPULSES_FOLDER = DATA_FOLDER / "sound-impulses"
